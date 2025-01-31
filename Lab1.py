68ee8823fbb67e68b822952850ee9e6235f50097
jw-xplore <jwernerltug@gmail.com>
2025-01-31 15:40:37 +0100

    Added single linked tree sorting test

--- Lab1.py
+++ Lab1.py
@@ -1,3 +1,9 @@
+import math
+
+#----------------------------------------
+# Single linked list link class
+#----------------------------------------
+
 class Link:
     number = 0
     link = None
@@ -5,6 +11,9 @@ class Link:
     def __init__(self, num):
         self.number = num
 
+#----------------------------------------
+# Single list functions
+#----------------------------------------
 
 def createLinkedList(*params):
 
@@ -40,22 +49,69 @@ def arrayInsertionSort(list):
             list[j - 1]
             list[j] = num
 
+
             j -= 1
+
+
+
+# Sort linked list segment
+def linkedListSortSegment(head: Link, compared: Link, position: int):
+    link: Link = head
+    startLink: Link = head
+    i = 0
+
+    middleI =  int(math.floor(position * 0.5))
+    halfSize = middleI
+
+    while link != None:
+        # Reaching mid point
+        if i == middleI:
+            # Debug print
+            # print(str(i) + ") pos = " + str(link.number))
+
+            # Setup starting point
+            if compared.number > link.number: # Right - start point is middle link
+                startLink = link
+                middleI += int(math.floor(halfSize * 0.5))
+            else:
+                middleI -= int(math.floor(halfSize * 0.5)) # Left - start point is same
+
+            sub = int(math.floor(halfSize * 0.5))
+            if sub < 1:
+                sub = 1
+            halfSize -= sub
+
+            # End placing
+            if halfSize == 0:
+                print("Place num: " + str(compared.number) + ", into i: " + str(i))
+                break
+
+            # Restart conting
+            link = startLink 
+            i = 0
+            continue
+
+        # Progress list
+        link = link.link
+        i += 1
 
 # Linked list insertion sort
 def linkedListInsertionSort(head: Link):
     link = head
     i = 0
 
+    print("Sorting----------------------")
+
     while link != None:
+        if i == 5:
+            linkedListSortSegment(head, link, i)
+        # Progress single level iteration
         link = link.link
         i += 1
-        j = i
-
-        # while j > 
 
 
 # Test list 
-singleLinkedList = createLinkedList(5, 4, 8, 21, 14, 3, 24, 18, 45, 100, 7, 62)
+singleLinkedList = createLinkedList(1, 3, 5, 7, 9, 18)
 iterateLinkedList(singleLinkedList)
+linkedListInsertionSort(singleLinkedList)
 
