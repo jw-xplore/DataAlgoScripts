c6035d80e8c589de47d939f2fc394f35b30a36ff
jw-xplore <jwernerltug@gmail.com>
2025-02-20 09:36:11 +0100

    functional lab2 p2 but with longer time

--- Lab2_p2.py
+++ Lab2_p2.py
@@ -1,48 +1,64 @@
-from enum import Enum
 
-class ESumType(Enum):
-    ROOT = 1
-    LEFT = 2
-    RIGHT = 3
+operation = 0
 
-def largestSubListSum(list, start = 0, type: ESumType = ESumType.ROOT):
+def largestSubListSum(list, start = 0):
 
-    leftSum = 0
-    rightSum = 0
+    leftRes = None
+    rightRes = None
+    lenL = len(list)
+    pivot: int = int(lenL / 2)
 
     # Split list
-    if (len(list) > 1):
-        pivot: int = int(len(list) / 2)
+    if (lenL > 1):
         # Left
         leftList = list[:pivot]
-        leftRes = largestSubListSum(leftList, start, ESumType.LEFT)
+        leftRes = largestSubListSum(leftList, start)
 
         # Right
         rightList = list[pivot:]
-        leftRes = largestSubListSum(rightList, start + pivot, ESumType.RIGHT)
+        rightRes = largestSubListSum(rightList, start + pivot)
 
-    # Summary
+    # Sum
     sum = 0
-    for i in list:
-        sum += i
+    if lenL == 1:
+        sum = list[0]
+    else:
+        # Get largest left value
+        leftLargestSum = 0
+        leftLargestSumTemp = 0
+        for i in range(pivot - 1, -1, -1):
+            leftLargestSumTemp += list[i]
+            if leftLargestSumTemp > leftLargestSum:
+                leftLargestSum = leftLargestSumTemp
+        
+        print("largest left: ", leftLargestSum)
 
-    # print
-    print(list, " = ", sum , " - ", start)
+        rightLargestSum = 0
+        rightLargestSumTemp = 0
+        for i in range(pivot, lenL, 1):
+            rightLargestSumTemp += list[i]
+            if rightLargestSumTemp > rightLargestSum:
+                rightLargestSum = rightLargestSumTemp
 
-    # Pick return sum
-    """
-    if type == ESumType.ROOT or type == ESumType.RIGHT: 
-        if sum < leftSum:
-            sum = leftSum
-    
-    if type == ESumType.ROOT or type == ESumType.LEFT: 
-        if sum < rightSum:
-            sum = rightSum
-    """
+        print("largest right: ", rightLargestSum)
 
-    return (sum, list, start)
+        # Get only positive or sum
+        if leftLargestSum > 0 and rightLargestSum > 0: # sum
+            sum = leftLargestSum + rightLargestSum
+        elif leftLargestSum > 0 and rightLargestSum < 0: # left
+            sum = leftLargestSum
+        elif leftLargestSum < 0 and rightLargestSum > 0: # right
+            sum = rightLargestSum
+        else:
+            sum = 0 # none
+
+        sum = leftLargestSum + rightLargestSum
+
+    print("sum of ", list, "is: ", sum)
+
+    return sum
 
 
-numbers = [ 1, 2, -5, 500, 1, 7, -90]
+numbers = [ -1, 2, -3, 4, 5, -4, 8]
 res = largestSubListSum(numbers)
 print("res: ", res)
