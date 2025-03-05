47b7ed1d6b371dbf68617a0405128b2c64fe4adf
jw-xplore <jwernerltug@gmail.com>
2025-03-05 16:12:14 +0100

    Added lab2 p2 description to code

--- Lab2_p2.py
+++ Lab2_p2.py
@@ -1,3 +1,78 @@
+import random
+
+#----------------------------------------
+# Descrition
+#----------------------------------------
+
+"""
+Program run steps:
+- Call function providing list of numbers 
+- Function call is self with sub list 
+- Left side from 0 to half size 
+- Right side from half size to end 
+- Call itself until provided sub list has only 1 item 
+- Return value of this one item 
+- Sum all left and right numbers 
+- Gain largest result at left side 
+    - Compare left largest value from left (including all negative numbers from left side) to left summary + right largest value from left 
+    - Pick bigger number 
+- Gain largest result at right side 
+    - Compare right largest value from right (including all negative numbers from right side) to right summary + left largest value from left 
+    - Pick bigger number 
+- Pick largest value from Sum, Largest left or Largest right results
+- Return tuple with containing Sum, Largest left, Largest right and Largest value
+    - These result are then used when comparing results of larger (next in recursion) sub list
+    - Largest value is then used as result
+    - Base case of recursion is when sublist has only 1 element. In that case tuple return value of the number for every of four values.
+
+
+Time complexity:
+Worst time complexity is O(n). Algorithm divides list down to single element and only combines results of tuple from left side with right side.
+For each call there are following comparisons done:
+- largest left results compared to left sum + right largest left result
+- largest right results compared to right sum + left largest right result
+- two comparisons to compare sum of left right largest result to right left largest result, with left largest sum and right largest sum
+
+So in the end there are 4 comparision to 7 different results (
+    overal sum
+    left largest
+    right largest
+    largest of sum
+    overal sum + right side left largest
+    overal sum + left side right largest
+    left side right largest + right side left largest
+    )
+"""
+
+#----------------------------------------
+# Create testing data
+#----------------------------------------
+
+def createTestData(size: int, randMin: int, randMax: int):
+    
+    file = open("testData.txt", "w")
+
+    for i in range(0, size):
+        number = random.randrange(randMin, randMax)
+        if number == 0:
+            number = random.choice([randMin, randMax])
+
+        file.write(str(number) + "\n")
+
+    file.close()
+
+def listFromTestData():
+    list = []
+    with open('testData.txt', 'r') as file:
+        for line in file:
+            list.append(int(line))
+
+    return list
+
+#----------------------------------------
+# Algorithm
+#----------------------------------------
+
 def largestSubListSum(list):
 
     lenL = len(list)
@@ -40,6 +115,8 @@ def largestSubListSum(list):
     return (sum, largestLeft, largestRight, largestSum)
 
 
-numbers = [ -2, 1, -3, 4, -1, 2, 1, -5, 4]
+createTestData(10, -10, 10)
+numbers = listFromTestData()
+print("Numbers: ", numbers)
 res = largestSubListSum(numbers)
-print("res: ", res[3])
+print("Res: ", res[3])
